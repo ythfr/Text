b7647f4e26065ce86deba7df5bf73cebe3c4c7ba
ythfr <mogendiokara@gmail.com>
2026-09-25 00:54:46 +0300

    fix: enable TypeScript Jest transformation

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,6 +1,11 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/*.test.ts'],
-  transform: {},
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json',
+    },
+  },
 };
